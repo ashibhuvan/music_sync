f160ee5f5be101649a684913f6d4d03ef6678355
ashi <ashi@ashis-MacBook-Air.local>
2019-10-06 17:55:43 -0400

    going to natively get the information instead of using python package which was causing problems

--- main3.py
+++ main3.py
@@ -1,15 +1,9 @@
+
 import sys
-import spotipy
-import spotipy.util as util
+
 
 #first task is to list all the playlists from a user
 #second task is to list the songs from a playlist link
 
 #third task to convert playlist to youtube
-
-def show_tracks(tracks):
-	for i, item in enumerate(tracks['items']):
-		track = item['track']
-print
-
 
