c8d2496e376d2a7b2553ae37f7b018467c407099
ashi <ashi@ashis-MacBook-Air.local>
2019-10-06 18:27:30 -0400

    successfully can get track lists from a playlist

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 
 import sys
-chnge
+import requests
+import json
+
+
+headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer BQB8dgxwNLy-B0lfPaxMVe4bv5tEkiXQP46YKx8iOPchkx-Tl8Ca7c2kPKoUqUvPfEUCm79yD6Prpzest3Up7JjV9nSLnsPoHyyan6spP-8t_ebArcTEHWHwKkMuwXVf4H-5tlzaAQa8kFHh',
+}
+
+
+params = (
+    ('fields', 'items(track(name,artists))'),
+    
+)
+
+response = requests.get('https://api.spotify.com/v1/playlists/6a1lf2JSEHBR9Rax025CdG/tracks', headers=headers, params=params)
+
+data = response.json()
+print(data)
 
 #first task is to list all the playlists from a user
 #second task is to list the songs from a playlist link
