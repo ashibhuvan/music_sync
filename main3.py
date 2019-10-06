94fc31cf64f63555a19c218d569e21f326cb99f3
ashi <ashi@ashis-MacBook-Air.local>
2019-10-06 13:06:42 -0400

    added some to do to follow for the mvp

--- main3.py
+++ main3.py
@@ -3,9 +3,9 @@ import spotipy
 import spotipy.util as util
 
 #first task is to list all the playlists from a user
+#second task is to list the songs from a playlist link
 
-#secondt basic task is too list all the songs from a playist
-
+#third task to convert playlist to youtube
 
 def show_tracks(tracks):
 	for i, item in enumerate(tracks['items']):
